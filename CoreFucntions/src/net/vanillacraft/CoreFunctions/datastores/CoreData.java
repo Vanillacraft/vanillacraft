daf2038e9ec6417478cfbc4c6c80c236ecbcbf6c
Ryan Reuther <ryan.reuther@gmail.com>
2015-05-07 18:34:41 -0700

    Updated to use UUIDs

--- CoreFucntions/src/net/vanillacraft/CoreFunctions/datastores/CoreData.java
+++ CoreFucntions/src/net/vanillacraft/CoreFunctions/datastores/CoreData.java
@@ -6,7 +6,10 @@ import net.vanillacraft.CoreFunctions.main.CoreFunctions;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import javax.print.attribute.standard.Finishings;
+import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
+import java.util.UUID;
 
 /**
  * Created by ryan on 5/5/2015.
@@ -18,8 +21,14 @@ public class CoreData {
     private CoreFunctions plugin;
     private ILogger logger;
 
-    private HashMap<Player, Long> teleportTimers = new HashMap<>();
-    private HashMap<Player, opMode> modMode = new HashMap<Player, opMode>();
+    private HashMap<UUID, Long> teleportTimers = new HashMap<>();
+    private HashMap<UUID, opMode> modMode = new HashMap<>();
+
+    private HashMap<UUID, Location> playerHomeLocations = new HashMap<>();
+    private HashMap<UUID, Long> playerSetHomeCoolDown = new HashMap<>();
+
+    private final long SETHOMECOOLDOWN = (1000 * 60) * 60;
+    private final long TELEPORTCOOLDOWN = (1000 * 60) * 30;
 
     private Location spawnLocation = new Location(plugin.getServer().getWorld("world"), 0,0,0);
 
@@ -34,7 +43,6 @@ public class CoreData {
         return logger;
     }
 
-
     public Database getDatabase()
     {
         return database;
@@ -45,23 +53,52 @@ public class CoreData {
         return spawnLocation;
     }
 
-    public boolean isModMode(Player player){
-        if(modMode.containsKey(player)){
-            if(modMode.get(player).isEnabled()){
+    public boolean isModMode(UUID uuid){
+        if(modMode.containsKey(uuid)){
+            if(modMode.get(uuid).isEnabled()){
                 return true;
             }
         }
         return false;
     }
 
-    public int getMinutesRemainTeleport(Player player){
-        if(teleportTimers.containsKey(player)){
-            long milsReamin = teleportTimers.get(player) - System.currentTimeMillis();
+    public int getMinutesRemainTeleport(UUID uuid){
+        if(teleportTimers.containsKey(uuid)){
+            long milsReamin = teleportTimers.get(uuid) - System.currentTimeMillis();
             int minRemain = (int)((milsReamin / 1000) / 60);
             return minRemain;
         } else {
             return 0;
         }
+    }
+
+    public Location getPlayerHomeLocation(UUID uuid) {
+        return playerHomeLocations.get(uuid);
+    }
+
+    public long getPlayerSetHomeCoolDown(UUID uuid) {
+        if(playerSetHomeCoolDown.containsKey(uuid)){
+           return playerSetHomeCoolDown.get(uuid);
+        } else {
+            return 0;
+        }
+    }
+
+    public void setPlayerHome(UUID uuid, Location location){
+
+        if(playerHomeLocations.containsKey(uuid)){
+            playerHomeLocations.remove(uuid);
+        }
+
+        if(playerSetHomeCoolDown.containsKey(uuid)){
+            playerSetHomeCoolDown.remove(uuid);
+        }
+
+        //TODO add method to write to the database
+        SetHomeRecord record = new SetHomeRecord(uuid, location);
+
+        playerSetHomeCoolDown.put(uuid, System.currentTimeMillis() + SETHOMECOOLDOWN);
+        playerHomeLocations.put(uuid, location);
     }
 
 }
