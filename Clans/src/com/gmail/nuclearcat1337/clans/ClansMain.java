27076e472dbab1e8e962db8f59baeea27f03085a
Eric <nuclearcat1337@gmail.com>
2015-05-27 19:58:41 -0700

    Inital commit for clans

---
+++ Clans/src/com/gmail/nuclearcat1337/clans/ClansMain.java
@@ -0,0 +1,21 @@
+package com.gmail.nuclearcat1337.clans;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+Created by Mr_Little_Kitty on 5/27/2015
+*/
+public class ClansMain extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
