0038ae714148078e7c7b45a4d1cafc04100189c7
Jiibrael <xdxjiibrael@gmail.com>
2015-05-28 23:08:38 -0400

    Added Global channel creation on plugin Enable and some first level channel functionalities

--- Chat/src/net/vanillacraft/Chat/main/Chat.java
+++ Chat/src/net/vanillacraft/Chat/main/Chat.java
@@ -1,9 +1,12 @@
 package net.vanillacraft.Chat.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 import net.vanillacraft.Chat.utils.Channel;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -13,16 +16,30 @@ public class Chat extends JavaPlugin {
 	
 	private static Chat instance;
 	
+	private HashMap<UUID, Channel> channelMap = new HashMap<UUID, Channel>();
 	private ArrayList<Channel> channelList = new ArrayList<>();
 	
 	@Override
 	public void onEnable(){
 		instance = this;
+		ArrayList<Player> allConnectedPlayers = 
+				new ArrayList<Player>(this.getServer().getOnlinePlayers());
+		Channel globalChannel = new Channel("Global", allConnectedPlayers);
+
+		channelList.add(globalChannel);
+		// TODO: puts all connected players UUID in the channelMap
+		for(Player p: allConnectedPlayers){
+			channelMap.put(p.getUniqueId(), globalChannel);
+		}
 	}
 	
 	@Override
 	public void onDisable(){
 		
+	}
+	
+	public Chat getInstance(){
+		return instance;
 	}
 
 }
